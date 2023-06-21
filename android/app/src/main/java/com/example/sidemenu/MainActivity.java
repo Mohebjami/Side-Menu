8d678311736a9a8625b81bdc2a9963dd77b21890
Moheb Jami <mohebjami2018@gmail.com>
2023-06-21 19:44:08 +0430

    My Code

---
+++ android/app/src/main/java/com/example/sidemenu/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.sidemenu;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,187 @@
+import 'package:flutter/material.dart';
+import 'package:shrink_sidemenu/shrink_sidemenu.dart';
+import 'package:icons_plus/icons_plus.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  bool isOpened = false;
+  var iconColor = Colors.black;
+  final GlobalKey<SideMenuState> _sideMenuKey = GlobalKey<SideMenuState>();
+  final GlobalKey<SideMenuState> _endSideMenuKey = GlobalKey<SideMenuState>();
+
+  toggleMenu([bool end = false]) {
+    if (end) {
+      final _state = _endSideMenuKey.currentState!;
+      if (_state.isOpened) {
+        _state.closeSideMenu();
+      } else {
+        _state.openSideMenu();
+      }
+    } else {
+      final _state = _sideMenuKey.currentState!;
+      if (_state.isOpened) {
+        _state.closeSideMenu();
+      } else {
+        _state.openSideMenu();
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SideMenu(
+      key: _sideMenuKey,
+      menu: buildMenu(),
+      type: SideMenuType.slideNRotate,
+      onChange: (_isOpened) {
+        setState(() => isOpened = _isOpened);
+      },
+      child: IgnorePointer(
+        ignoring: isOpened,
+        child: Scaffold(
+          body: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 40, left: 10, right: 10),
+                child: SizedBox(
+                  width: double.infinity,
+                  height: 30,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      IconButton(
+                        icon: const Icon(
+                    Icons.menu,
+                          color: Color.fromRGBO(17, 36, 115, 1),
+                          size: 35,
+                        ),
+                        onPressed: () => toggleMenu(),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Bootstrap.search,
+                            color: Color.fromRGBO(17, 36, 115, 1), size: 30),
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildMenu() {
+    return SingleChildScrollView(
+      padding: const EdgeInsets.symmetric(vertical: 50.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Padding(
+            padding: EdgeInsets.only(left: 16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CircleAvatar(
+                  backgroundColor: Colors.white,
+                  radius: 22.0,
+                ),
+                SizedBox(height: 16.0),
+                Text(
+                  "Hello, Moheb Jami",
+                  style: TextStyle(color: Colors.white),
+                ),
+                SizedBox(height: 20.0),
+              ],
+            ),
+          ),
+          ListTile(
+            onTap: () {},
+            leading: const Icon(Icons.home, size: 20.0, color: Colors.white),
+            title: const Text("Home"),
+            textColor: Colors.white,
+            dense: true,
+          ),
+          ListTile(
+            onTap: () {},
+            leading: const Icon(Icons.verified_user,
+                size: 20.0, color: Colors.white),
+            title: const Text("Profile"),
+            textColor: Colors.white,
+            dense: true,
+
+            // padding: EdgeInsets.zero,
+          ),
+          ListTile(
+            onTap: () {},
+            leading: const Icon(Icons.monetization_on,
+                size: 20.0, color: Colors.white),
+            title: const Text("Wallet"),
+            textColor: Colors.white,
+            dense: true,
+
+            // padding: EdgeInsets.zero,
+          ),
+          ListTile(
+            onTap: () {},
+            leading: const Icon(Icons.shopping_cart,
+                size: 20.0, color: Colors.white),
+            title: const Text("Cart"),
+            textColor: Colors.white,
+            dense: true,
+
+            // padding: EdgeInsets.zero,
+          ),
+          ListTile(
+            onTap: () {},
+            leading:
+            const Icon(Icons.star_border, size: 20.0, color: Colors.white),
+            title: const Text("Favorites"),
+            textColor: Colors.white,
+            dense: true,
+
+            // padding: EdgeInsets.zero,
+          ),
+          ListTile(
+            onTap: () {},
+            leading:
+            const Icon(Icons.settings, size: 20.0, color: Colors.white),
+            title: const Text("Settings"),
+            textColor: Colors.white,
+            dense: true,
+
+            // padding: EdgeInsets.zero,
+          ),
+        ],
+      ),
+    );
+  }
+}
